44a25ae5fc021141b92e4b74ece288c7e8e1c3eb
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2017-12-03 23:27:52 -0600

    run all changes

--- run_all.sh
+++ run_all.sh
@@ -2,21 +2,23 @@
 
 workloads=()
 #machine learning (mlpack)
-workloads += ('knn')
-workloads += ('nmf')
+workloads+=('knn')
+workloads+=('nmf')
 #database
-workloads += ('redis')
-workloads += ('sysbench')
+workloads+=('redis')
+workloads+=('sysbench')
 #map-reduce
-workloads += ('grep')
-workloads += ('wordcount')
+workloads+=('grep')
+workloads+=('wordcount')
 #graph-analytics
-workloads += ('galois-sssp')
-workloads += ('galois-pagerank')
+workloads+=('galois-sssp')
+workloads+=('galois-pagerank')
 #tail-bench
-workloads += ('moses')
-workloads += ('sphinx')
-workloads += ('img-dnn')
+workloads+=('moses')
+workloads+=('xapian')
+workloads+=('img-dnn')
+workloads+=('sphinx')
+workloads+=('slio')
 
 
 
@@ -25,11 +27,11 @@ do
 	touch ~/report_$workload.txt
 	if [ $workload == "knn" ]
 	then
-		python run_configurations.py -name $workload -c "/home/heidars2/mlpack/build/bin/mlpack_knn -r ../../../mlpack-benchmarks/datasets/mnist_all.csv -k 10 -v"
+		python run_configurations.py -name $workload -c "/home/heidars2/mlpack/build/bin/mlpack_knn -r /home/heidars2/mlpack-benchmarks/datasets/mnist_all.csv -k 10 -v"
 
 	elif [ $workload == "nmf" ]
 	then
-		python run_configurations.py -name $workload -c "/home/heidars2/mlpack/build/bin/mlpack_nmf -r 10 -i ~/mlpack-benchmarks/datasets/TomsHardware.csv
+		python run_configurations.py -name $workload -c "/home/heidars2/mlpack/build/bin/mlpack_nmf -r 10 -i /home/heidars2/mlpack-benchmarks/datasets/TomsHardware.csv
 "
 	elif [ $workload == "redis" ]
 	then
@@ -44,12 +46,28 @@ do
 		python run_configurations.py -name $workload -c "/home/josers2/BigDataBench_V3.2.1_Hadoop_Hive/MicroBenchmarks/run_MicroBenchmarks.sh 3"
 	elif [ $workload == "galois-sssp" ]
 	then
+		echo hi
 	elif [ $workload == "galois-pagerank" ]
 	then
-
+		echo hi
+	elif [ $worloads == "img-dnn" ]
+	then
+		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/img-dnn/run.sh"
 	elif [ $worloads == "moses" ]
 	then
-		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/img-dnn//home/josers2/BigDataBench_V3.2.1_Hadoop_Hive/MicroBenchmarks/run_MicroBenchmarks.sh 3"
-	fi
+		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/moses/run.sh"
+	
+	elif [ $worloads == "sphinx" ]
+	then
+		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/sphinx/run.sh"
+
+	elif [ $worloads == "xapian" ]
+	then
+		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/xapian/run.sh"
+
+	elif [ $worloads == "silo" ]
+	then
+		python run_configurations.py -name $workload -c "/home/alberto6/Tailbench/tailbench-v0.9/silo/run.sh"
+fi
 done
 
