944d0771841459c89b6b7fc6905817fd452cfbc9
Jose Rodrigo Sanchez Vicarte <josers2@illinois.edu>
2018-03-07 15:30:40 -0600

    broke into functions
    broke the scripts into functions, so they can be imported and called
    without using system commands.

--- run_configurations.py
+++ run_configurations.py
@@ -32,96 +32,75 @@ def parse():
     return options
 
 # co-location setting
-#masks=['0x00003', '0x00007','0x0000F',\
-#       '0x0001F', '0x0003F', '0x0007F', '0x000FF',\
-#       '0x001FF', '0x003FF', '0x007FF', '0x00FFF',\
-#       '0x01FFF', '0x03FFF', '0x07FFF', '0x0FFFF',\
-#       '0x1FFFF', '0x3FFFF']
-#
-#c_masks=['0xFFFFC', '0xFFFF8','0xFFFF0',\
-#       '0xFFFE0', '0xFFFC0', '0xFFF80', '0xFFF00',\
-#       '0xFFE00', '0xFFC00', '0xFF800', '0xFF000',\
-#       '0xFE000', '0xFC000', '0xF8000', '0xF0000',\
-#       '0xE0000', '0xC0000']
+# masks=['0x00003', '0x00007','0x0000F',\
+#        '0x0001F', '0x0003F', '0x0007F', '0x000FF',\
+#        '0x001FF', '0x003FF', '0x007FF', '0x00FFF',\
+#        '0x01FFF', '0x03FFF', '0x07FFF', '0x0FFFF',\
+#        '0x1FFFF', '0x3FFFF']
 
-# single setting
-masks=['0x00003', '0x00007','0x0000F',\
-       '0x0001F', '0x0003F', '0x0007F', '0x000FF',\
-       '0x001FF', '0x003FF', '0x007FF', '0x00FFF',\
-       '0x01FFF', '0x03FFF', '0x07FFF', '0x0FFFF',\
-       '0x1FFFF', '0x3FFFF', '0x7FFFF', '0xFFFFF']
+# c_masks=['0xFFFFC', '0xFFFF8','0xFFFF0',\
+#          '0xFFFE0', '0xFFFC0', '0xFFF80', '0xFFF00',\
+#          '0xFFE00', '0xFFC00', '0xFF800', '0xFF000',\
+#          '0xFE000', '0xFC000', '0xF8000', '0xF0000',\
+#          '0xE0000', '0xC0000']
 
-options=parse()
-#for num_partitions in range(18) : #change number of ways from 1 to 20
-num_cores=8
+def runConfigurations(fname, command):
+    # single setting
+    masks=['0x00003', '0x00007','0x0000F',\
+           '0x0001F', '0x0003F', '0x0007F', '0x000FF',\
+           '0x001FF', '0x003FF', '0x007FF', '0x00FFF',\
+           '0x01FFF', '0x03FFF', '0x07FFF', '0x0FFFF',\
+           '0x1FFFF', '0x3FFFF', '0x7FFFF', '0xFFFFF']
 
-try:
-	the_file = open("/home/josers2/report_{}.txt".format(options.name), 'w')
-except:
-	print("File cannot be opened")
+    #for num_partitions in range(18) : #change number of ways from 1 to 20
+    num_cores=8
 
-configurations=[2, 4, 6, 8, 10, 12, 14, 16, 18]
+    try:
+        the_file = open("/home/josers2/report_{}.txt".format(fname), 'w')
+    except:
+        print("File cannot be opened")
+        return
 
-for num_partitions in configurations:
-    latency=0
-    LLCMisses=0
-    IPC=0
-    partition_list_str = "llc:0={}".format(masks[num_partitions])
-    allocation_list_str ="llc:0={}-{}".format(0, num_cores)
-    #print "{}-{}".format(num_partitions, num_cores)
-    cmd= "sudo pqos -e {} -a {}".format(partition_list_str, allocation_list_str)
-    print cmd
-    os.system(cmd)
+    configurations=[2, 4, 6, 8, 10, 12, 14, 16, 18]
 
-    for i in range(5):
-        # thrash llc
-        cmd = "/home/josers2/thrash_cache.o"
+    for num_partitions in configurations:
+        latency=0
+        LLCMisses=0
+        IPC=0
+        partition_list_str = "llc:0={}".format(masks[num_partitions])
+        allocation_list_str ="llc:0={}-{}".format(0, num_cores)
+        #print "{}-{}".format(num_partitions, num_cores)
+        cmd= "sudo pqos -e {} -a {}".format(partition_list_str, allocation_list_str)
+        print cmd
         os.system(cmd)
 
-        log_name="log-{}-{}-{}.txt".format(num_partitions, num_cores, i)
-        cmd= "perf stat -e cycles,cpu-clock,cache-references,instructions,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o {} taskset -c {}-{} {}".format(log_name, 0, num_cores, options.command, log_name)
-        os.system(cmd)
-        cmd= "cat ./{}".format(log_name)
-        (out, err) = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
-        out=out.split("\n")[:-1]
-        m=re.search('\d*\.\d*', out[8])
-        #print m.group(0)
-        IPC+=float(m.group(0))
-        m=re.search('\d*\.\d*', out[10])
-        #print m.group(0)
-        LLCMisses+=float(m.group(0))
-        latency+=float(out[14].lstrip(' ').split(' ')[0])
-    IPC/=5
-    LLCMisses/=5
-    latency/=5
-    print "num_partition is: {} , latency is: {}".format(num_partitions, latency)
-    the_file.write("num_partition: {}\n".format(num_partitions))
-    the_file.write("IPC, MissRate, Latency: {}, {}, {}\n\n".format(IPC, LLCMisses, latency))
+        for i in range(5):
+            # thrash llc
+            cmd = "/home/josers2/thrash_cache.o"
+            os.system(cmd)
 
-the_file.close()
+            log_name="log-{}-{}-{}.txt".format(num_partitions, num_cores, i)
+            cmd= "perf stat -e cycles,cpu-clock,cache-references,instructions,LLC-loads,LLC-load-misses,LLC-stores,LLC-prefetches -o {} taskset -c {}-{} {}".format(log_name, 0, num_cores, command, log_name)
+            os.system(cmd)
+            cmd= "cat ./{}".format(log_name)
+            (out, err) = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+            out=out.split("\n")[:-1]
+            m=re.search('\d*\.\d*', out[8])
+            #print m.group(0)
+            IPC+=float(m.group(0))
+            m=re.search('\d*\.\d*', out[10])
+            #print m.group(0)
+            LLCMisses+=float(m.group(0))
+            latency+=float(out[14].lstrip(' ').split(' ')[0])
+        IPC/=5
+        LLCMisses/=5
+        latency/=5
+        print "num_partition is: {} , latency is: {}".format(num_partitions, latency)
+        the_file.write("num_partition: {}\n".format(num_partitions))
+        the_file.write("IPC, MissRate, Latency: {}, {}, {}\n\n".format(IPC, LLCMisses, latency))
 
+    the_file.close()
 
-#    if (LLCMisses > lower_bound * Ref_LLCMisses and LLCMisses < upper_bound * Ref_LLCMisses):
-#        #or (IPC > lower_bound * Ref_IPC and IPC < upper_bound * Ref_IPC):
-#        break;
-#    elif LLCMisses > upper_bound * Ref_LLCMisses:
-#        num_partitions += (18 - num_partitions)/ 2
-#    elif LLCMisses < lower_bound * Ref_LLCMisses:
-#        num_partitions -= 1
-#
-    #if latency > 1.05 * Ref_Latency and  latency < 1.075 * Ref_Latency:
-    #    # start gradient descent
-    #    num_partitions-=jumpSize
-    #    jumpSize/=2
-    #    if jumpSize == 0:
-    #        break
-    #elif latency > 1.075 * Ref_Latency:
-    #    num_partitions += jumpSize
-    #    jumpSize/=2
-    #    if jumpSize == 0:
-    #        break;
-    #else:
-    #    print "here!"
-    #    #break
-
-#print "Working set size is: {}".format(num_partitions)
+if __name__ == "__main__":
+    options=parse()
+    runConfigurations(options.name, options.command)
